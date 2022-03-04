060c29dce78e2c9efd96ac337aba71e3ab8406ff
DanielAmoudokpoJoovence <d.amoudokpo@joovence.com>
2022-03-04 20:51:54 +0100

    Implemantation  of login and of singleton

--- www/Core/Verificator.class.php
+++ www/Core/Verificator.class.php
@@ -2,7 +2,6 @@
 
 namespace App\Core;
 
-
 class Verificator
 {
 
@@ -10,57 +9,47 @@ class Verificator
     {
         $result = [];
         //Le nb de inputs envoyés ?
-        if( count($data) != count($config['inputs'])){
+        if (count($data) != count($config['inputs'])) {
             die("Tentative de hack !!!!");
         }
 
-        foreach ($config['inputs'] as $name=>$input){
+        foreach ($config['inputs'] as $name => $input) {
 
-            if(!isset($data[$name]) ){
-                $result[] = "Le champs ".$name." n'existe pas";
+            if (!isset($data[$name])) {
+                $result[] = "Le champs " . $name . " n'existe pas";
             }
 
-            if(empty($data[$name]) && !empty($input["required"]) ){
-                $result[] = "Le champs ".$name." ne peut pas être vide";
+            if (empty($data[$name]) && !empty($input["required"])) {
+                $result[] = "Le champs " . $name . " ne peut pas être vide";
             }
 
-            if($input["type"] == "email" && !self::checkEmail($data[$name]) ){
+            if ($input["type"] == "email" && !self::checkEmail($data[$name])) {
                 $result[] = $input["error"];
             }
 
-            if($input["type"] == "password" && empty($input["confirm"]) && !self::checkPassword($data[$name]) ){
+            if ($input["type"] == "password" && empty($input["confirm"]) && !self::checkPassword($data[$name])) {
                 $result[] = $input["error"];
             }
 
-            if(!empty($input["confirm"]) && $data[$name] != $data[$input["confirm"]]){
+            if (!empty($input["confirm"]) && $data[$name] != $data[$input["confirm"]]) {
                 $result[] = $input["error"];
             }
-
-
         }
 
-
         return $result;
-
     }
 
     public static function checkEmail($email): bool
     {
-       return filter_var($email, FILTER_VALIDATE_EMAIL);
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
     }
-
 
     public static function checkPassword($password): bool
     {
 
-        return strlen($password)>=8
+        return strlen($password) >= 8
             && preg_match("/[0-9]/", $password, $match)
             && preg_match("/[a-z]/", $password, $match)
-            && preg_match("/[A-Z]/", $password, $match)
-            ;
+            && preg_match("/[A-Z]/", $password, $match);
     }
-
-
-
-
 }
