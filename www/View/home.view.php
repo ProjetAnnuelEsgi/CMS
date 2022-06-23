6b7ff6b2c2f73ef34b6ef180dfc71224edaaf78a
earvin25 <earvin25@hotmail.fr>
2022-06-23 09:06:42 +0200

    design pages home login logout et register

--- www/View/home.view.php
+++ www/View/home.view.php
@@ -1,4 +1,4 @@
-<h1>Welcome</h1>
+<h1>Bienvenue sur notre CMS</h1>
 
 <?php
 
@@ -8,10 +8,23 @@ $auth = new Authenticator();
 $auth->authenticated(false);
 
 ?>
-
+<div class="home-button">
+<br>
 <a href="http://localhost/login">
   <button>Login</button>
 </a>
+
+<!-- Espace &#160 -->
+&#160;
+
 <a href="http://localhost/register">
   <button>Register</button>
 </a>
+</div>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../Style/home.css"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+</head>
