e9a9ebf9eb4b3e822bf585ad3684ab4df3128f04
danielamoudokpo <danielamoudokpo@gmail.com>
2022-07-13 18:36:05 +0200

    remove unused files

--- sql_files/esgi_user_table_update.sql
+++
@@ -1,6 +0,0 @@
-
--- Change status column to active
-ALTER TABLE `esgi_user` CHANGE `status` `active` TINYINT(4) NOT NULL DEFAULT '0';
-
--- Add roles column to user table
-ALTER TABLE `esgi_user` ADD `role` INT(2) NULL AFTER `active` NOT NULL DEFAULT '0';
