e67c35cdad4b4e6558adf832a35c90b096b2d8d3
danielamoudokpo <danielamoudokpo@gmail.com>
2022-09-02 17:59:26 +0200

    Change database and update docker-compose

--- sql/esgi_user.sql
+++ sql/esgi_user.sql
@@ -1,34 +1,26 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
+-- -------------------------------------------------------------
+-- TablePlus 4.8.0(432)
 --
--- Hôte : database
--- Généré le : mar. 12 juil. 2022 à 22:47
--- Version du serveur : 5.7.38
--- Version de PHP : 8.0.19
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- https://tableplus.com/
+--
+-- Database: mvcdocker2
+-- Generation Time: 2022-09-02 17:28:34.7120
+-- -------------------------------------------------------------
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Base de données : `mvcdocker2`
---
 
--- --------------------------------------------------------
-
---
--- Structure de la table `esgi_user`
---
-
+DROP TABLE IF EXISTS `esgi_user`;
 CREATE TABLE `esgi_user` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(320) NOT NULL,
   `password` varchar(255) NOT NULL,
   `firstname` varchar(50) DEFAULT NULL,
@@ -36,36 +28,23 @@ CREATE TABLE `esgi_user` (
   `role` int(2) NOT NULL DEFAULT '0',
   `token` char(255) DEFAULT NULL,
   `active` tinyint(4) NOT NULL DEFAULT '0',
+  `panel_id` varchar(14) NOT NULL,
   `activation_code` varchar(255) NOT NULL,
   `activated_at` timestamp NULL DEFAULT NULL,
   `reset_link_token` varchar(100) DEFAULT NULL,
   `activation_expiry` timestamp NULL DEFAULT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `updatedAt` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=95 DEFAULT CHARSET=latin1;
 
---
--- Index pour les tables déchargées
---
 
---
--- Index pour la table `esgi_user`
---
-ALTER TABLE `esgi_user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
 
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `esgi_user`
---
-ALTER TABLE `esgi_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
